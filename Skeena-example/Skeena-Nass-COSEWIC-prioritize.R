f2a5111c859b4cf2c3eef24c96fd1e17a7d27cbb
DylanMG <dylan.glaser1@ucalgary.ca>
2022-02-25 16:12:12 -0800

    dplyr-ize Brendan's original code. add some coments with ##

--- Skeena-example/Skeena-Nass-COSEWIC-prioritize.R
+++ Skeena-example/Skeena-Nass-COSEWIC-prioritize.R
@@ -1,91 +1,106 @@
 # code to read in, process, plot and calculate % change for Skeena and Nass sockeye 
 # data are from Charmaine Carr-Harris (Sockeye; DFO)
 # generation length is assumed to be 5-years for sockeye
+setwd('Skeena-example')
 
-#### load required libraries ----
+# load packages, data and reshape ---------------------------------------------------
 library(tidyverse)
-library(zoo)
 
-#### load and wrangle data ----
-skeena_sx_raw <- read.csv('data/TRTC-Results--Skeena-Nass.csv') # make sure values do not contain commas for thousands 
-cu_du_decoder <- read.csv('data/skeena-du-cu-decoder.csv') # read in manually made decoder for mapping DUs to CUs
-skeena_sx_raw[,1] <- gsub('.*_','', skeena_sx_raw[,1]) # remove "SX" from CU names
+skeena_sx_raw <- read.csv('data/TRTC-Results--Skeena-Nass.csv') %>%
+  mutate(CU = gsub('.*_','', CU)) #remove "SX" from CU names
 
-# merge datasets and filter
-du_cu_skeena <- left_join(x = skeena_sx_raw,
-                 y = cu_du_decoder,
-                 by = "CU")
+#read in manually made decoder for mapping DUs to CUs
+cu_du_decoder <- read.csv('data/skeena-du-cu-decoder.csv')
 
-SkeenaNassSX <- du_cu_skeena %>%
+SkeenaNassSX <- left_join(skeena_sx_raw, cu_du_decoder, by = "CU") %>%
   select(Year,TE,DU,CU,CU_Name) %>%
   drop_na(DU) 
 
-# create a total wild Babine time series
-SkeenaNassSX_Babine <- subset(SkeenaNassSX, CU_Name == "Babine-Early-Wild")
-SkeenaNassSX_Babine$CU_Name <- "Babine-Total-Wild"
-SkeenaNassSX_Babine$CU <- "L-21-02"
+rm(skeena_sx_raw, cu_du_decoder) #garbage out
+
+#create a total wild Babine time series - so confused what's going on between here and plots!
+SkeenaNassSX_Babine <- filter(SkeenaNassSX, CU_Name == "Babine-Early-Wild") %>% 
+  mutate(CU_Name = "Babine-Total-Wild", #why take early only then overwrite?
+         CU = "L-21-02")
+
 total_Babine_spw <- SkeenaNassSX %>% 
   filter(DU == 158) %>%
   group_by(Year) %>%
   mutate(total_TE = sum(TE)) %>%
   filter(CU_Name == "Babine-Early-Wild")
 SkeenaNassSX_Babine$TE <- total_Babine_spw$total_TE
-SkeenaNassSX <- rbind(SkeenaNassSX,SkeenaNassSX_Babine)
 
-# create a combined DU number + name identifier 
-SkeenaNassSX$du_cu <- paste(SkeenaNassSX$DU, SkeenaNassSX$CU_Name,sep = " ")
+SkeenaNassSX <- rbind(SkeenaNassSX,SkeenaNassSX_Babine) %>% #don't like to overwrite stuff
+  mutate(du_cu = paste(DU, CU_Name), 
+         LnSpw=log(TE))
 
 #### plots of spawners over time ----
 # plot RAW spawner abundance over time by DU 
-p <- ggplot(SkeenaNassSX, aes(x=Year,y=TE)) + 
-      geom_line(color="grey")+
-      geom_point()+
-      facet_wrap(~du_cu,scales="free_y")+
-      theme_bw()+
-      xlab("Year") +
-      ylab("Spawner abundance") +
-      coord_cartesian(ylim=c(0,NA), xlim=c(1960,2019))+
-      theme(text = element_text(size = 8),
-            strip.background = element_blank())
-
-jpeg("output/Skeena-Nass-sockeye-raw.jpeg", width = 10, height = 6, units = "in", res = 600)
-print(p)
-dev.off()
-
-# plot LOG transformed and NOT smoothed spawner abundance over time by DU 
-SkeenaNassSX_lm <- SkeenaNassSX %>% 
-  group_by(du_cu) %>% 
-  select(Year,TE,DU,du_cu) %>%
-  mutate(LnSpw=log(TE))
-
-SkeenaNassSX_lm_fit <- SkeenaNassSX_lm %>% 
-  mutate(last_yr=last(Year))%>%
-  filter(between(Year,2004,2019))
-
-p <- ggplot(SkeenaNassSX_lm, aes(x=Year,y=LnSpw)) + 
+ggplot(SkeenaNassSX, aes(x=Year,y=TE)) +
   geom_line(color="grey")+
-  geom_point()+
+  geom_point(na.rm = TRUE)+
   facet_wrap(~du_cu,scales="free_y")+
   theme_bw()+
-  xlab("Year") +
-  ylab("ln spawner abundance") +
-  coord_cartesian(xlim=c(1960,2019))+
+  labs(x = "Year", y = "Spawner abundance") +
+  coord_cartesian(ylim=c(0,NA), xlim=c(1960,2019))+
   theme(text = element_text(size = 8),
-        strip.background = element_blank())+
-  geom_smooth(data=SkeenaNassSX_lm_fit, method='lm', formula= y~x)
+        strip.background = element_blank())
+
+ggsave("output/Skeena-Nass-sockeye-raw.jpeg", width = 10, height = 6, units = "in")
+
+# plot LOG transformed and NOT smoothed spawner abundance over time by DU 
+SkeenaNassSX_lm_fit <- SkeenaNassSX %>% 
+  group_by(du_cu) %>% 
+  select(Year,LnSpw,DU,du_cu) %>%
+  mutate(last_yr=last(Year))%>% 
+  filter(between(Year,2004,2019)) ##note this isn't explicitly the last year WITH DATA! 
+                                    #it needs to "float" more
+
+ggplot(SkeenaNassSX, aes(x=Year,y=LnSpw)) + 
+  geom_line(color="grey") +
+  geom_point(na.rm = TRUE) +
+  facet_wrap(~du_cu, scales="free_y") +
+  theme_bw() +
+  labs(x = "Year", y = "ln spawner abundance") +
+  coord_cartesian(xlim=c(1960,2019)) +
+  theme(text = element_text(size = 8),
+        strip.background = element_blank()) +
+  geom_smooth(data=SkeenaNassSX_lm_fit, method='lm', se = FALSE, color = "red", 
+              na.rm = TRUE)
   
-jpeg("output/Skeena-Nass-sockeye-log-no-smooth.jpeg", width = 10, height = 6, units = "in", res = 600)
-print(p)
-dev.off()
+ggsave("output/Skeena-Nass-sockeye-log-no-smooth.jpeg", width = 10, 
+       height = 6, units = "in")
+
 
 #### calculate % change over time ----
 # calculate % change over most recent three generations (based on all or just most recent three gens with data), 
 # save in table with DU and recent generational mean spawner abundance
 
-SkeenaNassSXFilter <- SkeenaNassSX %>% # drop DUs without data
-  filter(DU != c(153),
-         DU != c(168),
-         DU != c(41)) 
+SkeenaNassSXFilter <- filter(SkeenaNassSX, !(DU %in% c(153, 168, 41))) #drop DUs w/o data
+
+#hol up - this should be robust, check it out with this chunk to make a rule:
+if(TRUE){
+  #need a helper fr next chunk
+  yr_helper <- SkeenaNassSX %>%
+    filter(!is.na(TE)) %>%
+    group_by(du_cu) %>%
+    filter(Year == max(Year)) %>%
+    rename(max_yr = Year) %>%
+    select(du_cu, max_yr)
+  
+  #how many of the last 15 years have escapement data? 
+  data_check <- left_join(SkeenaNassSX, yr_helper, by = "du_cu") %>%
+    group_by(du_cu) %>%
+    #what is the most recent year with data and the last 15 years 
+    filter(Year > (max_yr-15) & Year <= max_yr) %>% 
+    summarise(check_yrs = n(),
+           no_dat = sum(is.na(TE))) %>%
+    #label the pops brendan removed 
+    mutate(data_quality = ifelse(du_cu %in% c("153 Johnston", "168 Bear", 
+                                          "41 Skeena River-high interior"), 
+                                 "deficient", "OK")) 
+  #ok so it appears pops where there are 4+ NAs in the last 15 years get booted. 
+}
 
 summary_table <- matrix(NA,
                         nrow = length(unique(SkeenaNassSXFilter$du_cu)),
@@ -95,7 +110,6 @@ summary_table[,1] <- unique(SkeenaNassSXFilter$du_cu)
 
 for(i in unique(SkeenaNassSXFilter$du_cu)){
   data.in <- subset(SkeenaNassSXFilter, du_cu== i)
-  data.in$LnSpw <- log(data.in$TE)
   lst_index_yr <- last(which(!is.na(data.in$LnSpw)))
   first_index_yr <- lst_index_yr-15
   first_index_yr <- ifelse(first_index_yr < 0, 1,first_index_yr)
@@ -113,3 +127,26 @@ colnames(summary_table) <- c("DU", "% change (recent)", "% change (all)", "recen
 # export table as csv
 write.csv(summary_table, "output/skeena_nass_sx_no_smooth.perChange.csv", row.names = FALSE)
 
+
+#dylan way - well this isn't any sexier...
+summary_table <- NULL
+for(i in unique(SkeenaNassSXFilter$du_cu)){
+  data.in <- filter(SkeenaNassSXFilter, du_cu== i)
+  lst_index_yr <- last(which(!is.na(data.in$LnSpw)))
+  first_index_yr <- lst_index_yr-15
+  first_index_yr <- ifelse(first_index_yr < 0, 1,first_index_yr) #what's a case where this line could be important??
+  model <- lm(data.in$LnSpw[first_index_yr:lst_index_yr]  ~ data.in$Year[first_index_yr:lst_index_yr])
+  model_all <- lm(data.in$LnSpw  ~ data.in$Year)
+
+  summary_table <- bind_rows(summary_table, 
+                             data.frame(i, (exp(model$coefficients[2]*15)-1)*100,
+                             (exp(model_all$coefficients[2]*15)-1)*100,
+                             exp(last(na.omit(data.in$LnSpw))), 
+                             data.in$Year[lst_index_yr]))
+}
+
+colnames(summary_table) <- c("DU", "% change (recent)", "% change (all)", "recent abundance", "last year with data")
+rownames(summary_table) <- NULL
+
+summary_table <- summary_table %>%
+  #mutate_at(2:3, round(2)) #do this better 
